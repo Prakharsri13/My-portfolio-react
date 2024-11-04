348b9a751932e4850000d0aba1a22c00f0b05a66
Prakharsri05 <86249065+Prakharsri13@users.noreply.github.com>
2024-11-04 23:04:38 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid-cols-[1fr,3rf,1fr]">
                 <h1 >
                     <a href="/" className="logo">
-                        <img src="/public/images/canvaprakhar.png" className="bg-white rounded-md p-1" width={40} height={40} alt="Prakhar Srivastava" />
+                        <img src="/public/images/1logo.png" className="bg-white rounded-md p-1" width={40} height={40} alt="Prakhar Srivastava" />
                     </a>
                 </h1>
 
